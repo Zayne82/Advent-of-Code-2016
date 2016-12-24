d6f840701fa2654dbdc681120d14a002f48192a0
Mikael Cederlöf <mikael.cederlof@gmail.com>
2016-12-24 12:06:01 +0100

    Created methods for extracting relevant information.
    Now I just needs to debug the last one.

--- src/Day7_1.java
+++ src/Day7_1.java
@@ -5,8 +5,12 @@ public class Day7_1 {
 		String example2 = "abcd[bddb]xyyx"; 		//False
 		String example3 = "aaaa[qwer]tyui"; 		//False
 		String example4 = "ioxxoj[asdfgh]zxcvbn"; 	//True
+		String example5 = "[mikael][cederlof][malin]cederlof[slut]";
 		
-		
+		System.out.println("Find Hypernet sequences.");
+		getHypernetSequences(example4);
+		System.out.println("Find non Hypernet sequences.");
+		getNonHypernetParts(example1);
 		/*
 		if(checkString("ioxxoj")){
 			System.out.println("True");
@@ -15,7 +19,8 @@ public class Day7_1 {
 		}
 		*/
 	}
-	boolean checkString(String input){
+	
+	private boolean checkString(String input){
 		for(int i = 0; i < input.length() - 3; i++){
 			if(		input.charAt(i) 	== input.charAt(i+3) && 
 					input.charAt(i+1) 	== input.charAt(i+2) && 
@@ -24,6 +29,42 @@ public class Day7_1 {
 			}
 		}
 		return false;
+	}
+	
+	private String[] getHypernetSequences(String input){
+		int numHyp = 0;
+		int pos = -1;
+		while((pos = input.indexOf("[", pos + 1)) != -1){
+			numHyp++;
+		}
+		String[] result = new String[numHyp];
+		pos = 0;
+		for(int i = 0; i < numHyp; i++){
+			result[i] = input.substring(pos = input.indexOf("[", pos) + 1, pos = input.indexOf("]", pos));
+			System.out.println("Found Hypernet sequence: " + result[i]);
+		}
+		return result;
+	}
+	
+	private String[] getNonHypernetParts(String input){
+		String[] parts = input.split("\\[");
+		int numParts = 0;
+		for(int i = 0; i < parts.length; i++){
+			if(!parts[i].endsWith("]")){
+				numParts++;
+			}
+		}
+		System.out.println("Found " + numParts + " sequences.");
+		String[] result = new String[numParts];
+		for(int i = 0; i < parts.length; i++){
+			if(!parts[i].endsWith("]")){
+				System.out.println("Found a non hypernet sequence.");
+				result[numParts - 1] = parts[i];
+				System.out.println("Non Hypernet sequence: " + result[numParts]);
+				numParts--;
+			}
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
